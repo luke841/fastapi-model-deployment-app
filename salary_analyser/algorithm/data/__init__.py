cdad708c7306031be63d4c67fec284a8e4d3c487
Luke-icetana <luke@icetana.com.au>
2023-05-27 15:23:32 +0800

    create cleanse csv script

--- salary_analyser/algorithm/data/__init__.py
+++ salary_analyser/algorithm/data/__init__.py
@@ -1,9 +1,8 @@
 from .census_record import CensusRecord
-from .preprocessing import cleanse_census_csv
-from .processing import process_data
+from .processing import process_data, clean_census_records
 from .utils import (
     dicts_to_dataclass, 
     read_csv, 
-    replace_string_in_dict_keys, 
+    replace_string_in_dict_keys,
     write_dataclasses_to_csv,
 )
