cb57a80bc7106a53994c3b50920a1a194d38fe89
Luke-icetana <luke@icetana.com.au>
2023-05-29 07:36:58 +0800

    add tests for data utilities

--- salary_analyser/algorithm/data/utils.py
+++ salary_analyser/algorithm/data/utils.py
@@ -1,18 +1,17 @@
 import csv
-from copy import deepcopy
 from pathlib import Path
 
-from dataclasses import asdict
+from pydantic import BaseModel
 
 
-def read_csv(path) -> list[dict]:
+def read_csv(path: Path) -> list[dict]:
     with open(path, "r") as fp:
         reader = csv.DictReader(fp, delimiter=',', skipinitialspace=True)
         census_values = list(reader)
     return census_values
 
 
-def write_dataclasses_to_csv(csv_file: str, dataclass_list: list):
+def write_dataclasses_to_csv(csv_file: Path, dataclass_list: list[BaseModel]):
     fieldnames = dataclass_list[0].dict(by_alias=True).keys()
     
     with open(csv_file, "w", newline="") as file:
@@ -21,5 +20,5 @@ def write_dataclasses_to_csv(csv_file: str, dataclass_list: list):
         for record in dataclass_list:
             writer.writerow(record.dict(by_alias=True))
 
-def dicts_to_dataclass(records: list[dict], DataClass):
-    return list(map(lambda record: DataClass(**record), records))
+def dicts_to_dataclass(records: list[dict], dataclass: BaseModel):
+    return list(map(lambda record: dataclass(**record), records))
