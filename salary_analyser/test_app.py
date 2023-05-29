f5780d3d59b81fc7f5b90fafa5ef45d7f6c1dba0
Luke-icetana <luke@icetana.com.au>
2023-05-30 07:09:38 +0800

    Update test_app.py

--- salary_analyser/test_app.py
+++ salary_analyser/test_app.py
@@ -1,36 +1,38 @@
+# TODO: get this to run in the tests/ folder
+
+
 import json
 
 from fastapi.testclient import TestClient
 
-from salary_analyser.algorithm.data.utils import dicts_to_dataclass, read_csv, write_dataclasses_to_csv
-# from salary_analyser.main import app
+from salary_analyser.main import app
 
-# client = TestClient(app)
+client = TestClient(app)
 
 
-# def test_index():
-#     r = client.get("/")
-#     print(r.json())
-#     assert r.json() == {"greetings": "pass the inference request to '/inference'"}
+def test_index():
+    r = client.get("/")
+    print(r.json())
+    assert r.json() == {"greetings": "pass the inference request to '/inference'"}
 
-# def test_inference():
-#     record = {
-#         'age': 39,
-#         'workclass': 'State-gov',
-#         'fnlgt': 77516,
-#         'education': 'Bachelors',
-#         'education-num': 13,
-#         'marital-status': 'Never-married',
-#         'occupation': 'Adm-clerical',
-#         'relationship': 'Not-in-family',
-#         'race': 'White',
-#         'sex': 'Male',
-#         'capital-gain': 2174,
-#         'capital-loss': 0,
-#         'hours-per-week': 40,
-#         'native-country': 'United-States',
-#  }
+def test_inference():
+    record = {
+        'age': 39,
+        'workclass': 'State-gov',
+        'fnlgt': 77516,
+        'education': 'Bachelors',
+        'education-num': 13,
+        'marital-status': 'Never-married',
+        'occupation': 'Adm-clerical',
+        'relationship': 'Not-in-family',
+        'race': 'White',
+        'sex': 'Male',
+        'capital-gain': 2174,
+        'capital-loss': 0,
+        'hours-per-week': 40,
+        'native-country': 'United-States',
+ }
     
-#     data = json.dumps(record)
-#     r = client.post("/inference", data=data)
-#     assert r.json() == {'salary': '<=50K'}
+    data = json.dumps(record)
+    r = client.post("/inference", data=data)
+    assert r.json() == {'salary': '<=50K'}
