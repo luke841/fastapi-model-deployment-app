9a65be0551eab0dcb3735e3653a843334361d70e
Luke-icetana <luke@icetana.com.au>
2023-05-30 06:51:03 +0800

    update salary tests

--- salary_analyser/main.py
+++ salary_analyser/main.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 from salary_analyser.algorithm.data import CensusRecord, InferenceResult
 from salary_analyser.algorithm.models import load_model
+from salary_analyser.algorithm.settings.settings import DEFAULT_MODEL_PATH
 
 # TODO: setup default model location using relative path
-# from salary_analyser.algorithm.settings.settings import DEFAULT_MODEL_LOCATION
-DEFAULT_MODEL_LOCATION = "../files/model/census_model.pkl"
+DEFAULT_MODEL_LOCATION = DEFAULT_MODEL_PATH
 
 
 app = FastAPI()
